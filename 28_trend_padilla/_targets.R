2fa3107d3dd92d70ee82f9f4ca31a43ac049ad5a
Julie Padilla <padilla410@gmail.com>
2023-04-18 16:36:02 -0700

    add targets

---
+++ 28_trend_padilla/_targets.R
@@ -0,0 +1,17 @@
+library(targets)
+library(tarchetypes)
+
+tar_option_set(packages = c(
+  "tidyverse", 
+  "dataRetrieval", 
+  "patchwork",
+  "sf"
+  ))
+
+# Phase target makefiles
+source("01_fetch.R")
+source("02_process.R")
+source("03_viz.R")
+
+# Combined list of target outputs
+c(p1_targets, p2_targets, p3_targets)
