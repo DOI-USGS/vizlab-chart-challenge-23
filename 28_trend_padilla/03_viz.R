e1b7e5ddfa9db82f8a1bee01e8c601c64a971b70
Julie Padilla <padilla410@gmail.com>
2023-04-19 11:10:04 -0700

    update targets:

--- 28_trend_padilla/03_viz.R
+++ 28_trend_padilla/03_viz.R
@@ -9,20 +9,19 @@ p3_targets <- list(
   tar_target(
     p3_great_lakes_maps,
     create_great_lakes_maps(in_zips = p1_gl_gis, homes_order = TRUE)
-  )#,
+  ),
   
-  # # make ice plots
-  # tar_target(
-  #   p3_max_ice_timeseries,
-  #   annual_max_ice_plot(ice_tibble = p2_ice_data)
-  # ),
-  # 
-  # tar_target(
-  #   p3_final_plot,
-  #   create_complete_plot(
-  #     map_plot = p3_gl_basin_map,
-  #     data_plot = p3_max_ice_timeseries,
-  #     ttl = "Trends in Maximum Percent Ice Cover \nfor the Great Lakes \n(1973-2023)"
-  #   )
-  # )
+  # make ice plots
+  tar_target(
+    p3_max_ice_timeseries,
+    annual_max_ice_plot(ice_tibble = p2_ice_data)
+  ),
+
+  tar_target(
+    p3_ls_combo_plots,
+    create_final_plot(
+      map = p3_great_lakes_maps,
+      timeseries = p3_max_ice_timeseries
+    )
+  )
 )
