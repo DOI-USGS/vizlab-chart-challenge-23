be1f17f1f8a539eb5ddd241b7b2a43fbc6b3df58
Margaux Sleckman <msleckman@usgs.gov>
2023-04-25 10:10:13 -0700

    adding functions

---
+++ 28_trends_msleckman/src/funs.R
@@ -0,0 +1,30 @@
+######################
+# Functions for Chart Challenge Pipeline Viz
+######################
+
+
+# source ------------------------------------------------------------------
+
+source(config.R)
+
+
+# processing functions ----------------------------------------------------
+
+
+# Viz functions -----------------------------------------------------------
+
+#' @title 
+#' @description 
+#' @param 
+#' @param 
+#' @param 
+#' @example 
+
+time_series_plot(data, date_range){
+  
+  data 
+  
+}
+
+
+map
